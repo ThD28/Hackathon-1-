b13c7ddecf6cda2b4ce82868de67645820022b92
joryan alves_barros <joryan.baros@gmail.com>
2022-11-24 11:09:23 +0100

    supp

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+    <section id="telnav">
+      <nav className="navbar">
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round" />
+        </label>
+        <ul className="navbar__links slideInDown  ">
+          <li className="navbar__item">
+            Destination
+          </li>
+          <li className="navbar__item">
+            Voyage
+          </li>
+        </ul>
+        <button type="button" className="navbar__burger">
+          <span className="burger__bar"> </span>
+        </button>
+      </nav>
+    </section>
+  );
+};
+
+export default Header;
