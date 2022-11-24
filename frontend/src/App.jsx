eb1559f6501d1071ead2b1a269613ef8542918fb
joryan alves_barros <joryan.baros@gmail.com>
2022-11-24 11:42:58 +0100

    first view header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Japon from "@pages/Japon";
 import Voyage from "@pages/Voyage";
-import Home from "@pages/Home";
-
-import "./App.css";
 import Header from "@components/Header";
+// import Home from "@pages/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header />
       <Routes>
         <Route exact path="/" element={<Japon />} />
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,15 @@
 import "./header.css";
+import logoAvion from "@assets/avion.png";
 
 const Header = () => {
   return (
     <section id="telnav">
       <nav className="navbar">
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round" />
-        </label>
-        <ul className="navbar__links slideInDown  ">
+        <img src={logoAvion} alt="avion" />
+        <ul className="navbar__links_slideInDown">
           <li className="navbar__item">Destination</li>
           <li className="navbar__item">Voyage</li>
         </ul>
-        <button type="button" className="navbar__burger">
-          <span className="burger__bar"> </span>
-        </button>
       </nav>
     </section>
   );
